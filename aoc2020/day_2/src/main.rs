e529b38d8994d53f76ef87674594c39f92b00560
Karlo Josip Kardum <kjkardum@gmail.com>
2021-06-19 15:33:03 +0200

    Day2 rusty rewrite

--- aoc2020/day_2/src/main.rs
+++ aoc2020/day_2/src/main.rs
@@ -5,30 +5,35 @@ fn main() {
     let mut part1_counter = 0;
     let mut part2_counter = 0;
     for line in lines {
-        let splitted = line
-            .split(|c| c == ':' || c == ' ' || c == '-')
-            .take(5)
-            .collect::<Vec<&str>>();
-        if let [min, max, letter, _, password] = &splitted[..] {
-            let occurences = password.matches(letter).count();
-            if (occurences as i32) >= min.parse::<i32>().unwrap()
-                && (occurences as i32) <= max.parse::<i32>().unwrap()
-            {
-                part1_counter += 1;
-            }
-            let first = (password.as_bytes()[(min.parse::<i32>().unwrap() as usize) - 1] as char)
-                .to_string()
-                .eq(letter);
-            let second = (password.as_bytes()[(max.parse::<i32>().unwrap() as usize) - 1] as char)
-                .to_string()
-                .eq(letter);
-            if (first as i32) + (second as i32) == 1 {
-                part2_counter += 1
-            }
-        }
+        parse_line(&line[..], &mut part1_counter, &mut part2_counter);
     }
     println!("Part 1: {}", part1_counter);
     println!("Part 2: {}", part2_counter);
+}
+
+fn parse_line(line: &str, part1_counter: &mut i32, part2_counter: &mut i32) -> Option<i32> {
+    let mut split_iter = line.split(|c| c == ':' || c == ' ' || c == '-');
+
+    let min = split_iter.next()?.parse::<i32>().ok()?;
+    let max = split_iter.next()?.parse::<i32>().ok()?;
+    let letter = split_iter.next()?;
+    split_iter.next()?;
+    let password = split_iter.next()?;
+
+    let occurences = password.matches(letter).count();
+    if (occurences as i32) >= min && (occurences as i32) <= max {
+        *part1_counter += 1;
+    }
+    let first = (password.as_bytes()[(min as usize) - 1] as char)
+        .to_string()
+        .eq(letter);
+    let second = (password.as_bytes()[(max as usize) - 1] as char)
+        .to_string()
+        .eq(letter);
+    if first ^ second {
+        *part2_counter += 1
+    }
+    Some(0)
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
