6bbc835928157b5e38dcb21e0288cce738af9dd7
Karlo Josip Kardum <kjkardum@gmail.com>
2021-06-19 15:07:11 +0200

    Moved from rustc to cargo build, added rustfmt

--- aoc2020/day_2/src/main.rs
+++ aoc2020/day_2/src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs::File,
-    io::prelude::*,
-    io::BufReader,
-};
+use std::{fs::File, io::prelude::*, io::BufReader};
 
 fn main() {
     let lines = read_lines("input.txt");
@@ -10,25 +6,22 @@ fn main() {
     let mut part2_counter = 0;
     for line in lines {
         let splitted = line
-            .split(|c| c==':' || c==' ' || c=='-')
+            .split(|c| c == ':' || c == ' ' || c == '-')
             .take(5)
             .collect::<Vec<&str>>();
-        if let [min, max, letter, _, password] = &splitted[..]{
-            let occurences =  password.matches(letter).count();
-            if (occurences as i32)>=min.parse::<i32>().unwrap() &&
-                (occurences as i32)<=max.parse::<i32>().unwrap() {
-                    part1_counter += 1;
+        if let [min, max, letter, _, password] = &splitted[..] {
+            let occurences = password.matches(letter).count();
+            if (occurences as i32) >= min.parse::<i32>().unwrap()
+                && (occurences as i32) <= max.parse::<i32>().unwrap()
+            {
+                part1_counter += 1;
             }
-            let first = (password.
-                            as_bytes()
-                            [(min.parse::<i32>().unwrap() as usize)-1]
-                                as char)
-                            .to_string().eq(letter);
-            let second = (password.
-                            as_bytes()
-                            [(max.parse::<i32>().unwrap() as usize)-1]
-                                as char)
-                            .to_string().eq(letter);
+            let first = (password.as_bytes()[(min.parse::<i32>().unwrap() as usize) - 1] as char)
+                .to_string()
+                .eq(letter);
+            let second = (password.as_bytes()[(max.parse::<i32>().unwrap() as usize) - 1] as char)
+                .to_string()
+                .eq(letter);
             if (first as i32) + (second as i32) == 1 {
                 part2_counter += 1
             }
@@ -41,7 +34,8 @@ fn main() {
 fn read_lines(filename: &str) -> Vec<String> {
     let file = File::open(filename).expect("No filename provided");
     let reader = BufReader::new(file);
-    reader.lines()
-          .map(|l| l.expect("Can't read line"))
-          .collect()
+    reader
+        .lines()
+        .map(|l| l.expect("Can't read line"))
+        .collect()
 }
